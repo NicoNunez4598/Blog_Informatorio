4015a1c934b523abcf3d3d597a3a5a514c97eabe
José Nicolás Núñez <nunezjosenicolas4598@gmail.com>
2024-09-09 11:59:02 -0300

    Creacion de los modelos y del usuario django

--- Blog_Online/GestionDeBlog/models.py
+++ Blog_Online/GestionDeBlog/models.py
@@ -12,7 +12,7 @@ class Categoria(models.Model):
     class Meta:
 
         verbose_name = 'Categoria'
-        verbose_name_plural = 'Categoria'
+        verbose_name_plural = 'Categorias'
     
     def __str__(self):
         
@@ -20,4 +20,22 @@ class Categoria(models.Model):
 
 class Autor(models.Model):
 
+    id = models.AutoField(primary_key=True)
+    nombres = models.CharField('Nombres del Autor', max_length=255, null=False, blank=False)
+    apellidos = models.CharField('Apellidos del Autor', max_length=255, null=False, blank=False)
+    facebook = models.URLField('Facebook', null=True, blank=True)
+    twitter = models.URLField('Twitter', null=True, blank=True)
+    instagram = models.URLField('Instagram', null=True, blank=True)
+    web = models.URLField('Web', null=True, blank=True)
+    correo = models.EmailField('Correo Electronico', null=False, blank=False)
+    estado = models.BooleanField('Autor Activo / Autor No Activo', default=True)
+    fecha_creacion = models.DateField('Fecha de Creacion', auto_now=False, auto_now_add=True)
+
+    class Meta:
+
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
     
+    def __str__(self):
+        
+        return "{0},{1}".format(self.apellidos, self.nombres)
