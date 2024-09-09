ba63ca0d63cb146b49a2128f455d6a5c448ba286
José Nicolás Núñez <nunezjosenicolas4598@gmail.com>
2024-09-09 19:21:14 -0300

    Configurando con Bootstrap

--- Blog_Online/Blog_Online/settings.py
+++ Blog_Online/Blog_Online/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'Blog_Online.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
